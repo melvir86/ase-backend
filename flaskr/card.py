70d7f14e4f84ed544f6e0d24738abd0e742ab77f
MELVIR SINGH <msingh09@student.bbk.ac.uk>
2023-06-28 22:14:23 +0000

    Completed rearchitecting card module backend

--- flaskr/card.py
+++ flaskr/card.py
@@ -7,6 +7,8 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 import folium
+import json
+from flask import jsonify
 
 bp = Blueprint('card', __name__)
 
@@ -14,27 +16,31 @@ bp = Blueprint('card', __name__)
 def index():
     return render_template('card/index.html')
 
-@bp.route('/listCard')
+@bp.route('/api/listCard', methods=('GET', 'POST'))
 def listCard():
     db = get_db()
+
     cards = db.execute(
         'SELECT *'
         ' FROM card c JOIN user u ON c.user_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('card/list.html', cards=cards)
 
-@bp.route('/createCard', methods=('GET', 'POST'))
-@login_required
+    return json.dumps([dict(ix) for ix in cards], indent=4, sort_keys=True, default=str)
+
+@bp.route('/api/createCard', methods=('GET', 'POST'))
 def createCard():
+    data = request.get_json()
+
     if request.method == 'POST':
-        name = request.form['name']
-        number = request.form['number']
-        expiry_month = request.form['expiry_month']
-        expiry_year = request.form['expiry_year']
-        cve = request.form['cve']
-        description = request.form['description']
-        status = request.form['status']
+        uid = data.get('uid')
+        name = data.get('name')
+        number = data.get('number')
+        expiry_month = data.get('expiry_month')
+        expiry_year =  data.get('expiry_year')
+        cve = data.get('cve')
+        description = data.get('description')
+        status = data.get('status')
         error = None
 
         if not name:
@@ -49,43 +55,59 @@ def createCard():
             db.execute(
                 'INSERT INTO card (user_id, name, number, expiry_month, expiry_year, cve, description, status)'
                 ' VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                (g.user['id'], name, number, expiry_month, expiry_year, cve, description, status)
+                (uid, name, number, expiry_month, expiry_year, cve, description, status)
             )
             db.commit()
-            return redirect(url_for('card.listCard'))
+            return json.dumps({'success':True}), 201, {'ContentType':'application/json'}
 
-    return render_template('card/create.html')
+@bp.route('/api/<int:id>/getCard', methods=('GET', 'POST'))
+def getCard(id):
 
-def get_card(id, check_author=True):
-    post = get_db().execute(
+    card = get_db().execute(
+        'SELECT *'
+        ' FROM card c JOIN user u ON c.user_id = u.id'
+        ' WHERE c.id = ?',
+        (id,)
+    ).fetchall()
+
+    if card is None:
+        abort(404, f"Card id {id} doesn't exist.")
+
+    return json.dumps([dict(ix) for ix in card], indent=4, sort_keys=True, default=str)
+
+def get_card_local(id, check_author=True):
+    data = request.get_json()
+    if data.get('id'):
+        id = data.get('id')
+
+    card = get_db().execute(
         'SELECT *'
         ' FROM card c JOIN user u ON c.user_id = u.id'
         ' WHERE c.id = ?',
         (id,)
     ).fetchone()
 
-    if post is None:
+    if card is None:
         abort(404, f"Card id {id} doesn't exist.")
 
-    if check_author and post['user_id'] != g.user['id']:
-        abort(403)
+    return card
 
-    return post
-
-@bp.route('/<int:id>/updateCard', methods=('GET', 'POST'))
-@login_required
+@bp.route('/api/<int:id>/updateCard', methods=('GET', 'POST'))
 def updateCard(id):
-    card = get_card(id)
+    data = request.get_json()
 
     if request.method == 'POST':
-        name = request.form['name']
-        number = request.form['number']
-        expiry_month = request.form['expiry_month']
-        expiry_year = request.form['expiry_year']
-        cve = request.form['cve']
-        description = request.form['description']
-        status = request.form['status']
+        uid = data.get('uid')
+        name = data.get('name')
+        number = data.get('number')
+        expiry_month = data.get('expiry_month')
+        expiry_year =  data.get('expiry_year')
+        cve = data.get('cve')
+        description = data.get('description')
+        status = data.get('status')
         error = None
+
+        card = get_card_local(id)
 
         if not name:
             error = 'Name is required.'
@@ -102,15 +124,11 @@ def updateCard(id):
                 (name, number, expiry_month, expiry_year, cve, description, status, id)
             )
             db.commit()
-            return redirect(url_for('card.listCard'))
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-    return render_template('card/update.html', card=card)
-
-@bp.route('/<int:id>/deleteCard', methods=('POST',))
-@login_required
+@bp.route('/api/<int:id>/deleteCard', methods=('POST',))
 def deleteCard(id):
-    get_card(id)
     db = get_db()
     db.execute('DELETE FROM card WHERE id = ?', (id,))
     db.commit()
-    return redirect(url_for('card.listCard'))
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
