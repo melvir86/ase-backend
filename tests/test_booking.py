0dfe8b66421dc7f184fdf5793ac87dedca0b53cd
MELVIR SINGH <msingh09@student.bbk.ac.uk>
2023-07-12 22:01:50 +0000

    Updated booking tests

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -27,6 +27,7 @@ def test_show_cars_success(client):
 
 def test_bookcar(client):
     # Prepare a JSON payload for the request
+    #TestUser ID is 6 in schema
     payload = {
         'uid': 6,
         'source': 'Manchester',
@@ -39,11 +40,12 @@ def test_bookcar(client):
     # Check that the response status code is 200 (OK)
     assert response.status_code == 200
     assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
-
+    
     # Add additional assertions to validate the response data or behavior
 
     # Check that the booking was inserted into the database
     booking = json.loads(response.get_data(as_text=True))
+    #print("Booking response")
     #print(booking)
 
     assert booking is not None
@@ -66,20 +68,17 @@ def test_listRequests(client):
     # Check that the booking was inserted into the database
     customer_requests = json.loads(response.get_data(as_text=True))
     print(customer_requests)
-    print(response.data)
+    #print(response.data)
 
-    assert b'Manchester' in response.data
-    assert b'Liverpool' in response.data
+    assert customer_requests[0]["status"] == 'Booked'
+    assert customer_requests[0]["user_id"] == 6
+    assert customer_requests[0]["source"] == 'Manchester'
 
 def test_acceptJob(client):
     # Prepare params for the request
 
-    payload = {
-        'carid': '3'
-    }
-
     # Send a POST request to the bookcar endpoint with the payload
-    response = client.post('/api/1/acceptJob', json=payload)
+    response = client.post('/api/1/acceptJob?carid=3')
 
     # Check that the response status code is 200 (OK)
     assert response.status_code == 200
@@ -91,3 +90,24 @@ def test_acceptJob(client):
     jobs = json.loads(response.get_data(as_text=True))
 
     assert b'success' in response.data
+
+def test_checkBooking(client):
+    # Prepare a JSON payload for the request
+
+    # Send a POST request to the bookcar endpoint with the payload
+    response = client.post('/api/checkBooking?uid=6')
+
+    # Check that the response status code is 200 (OK)
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
+
+    # Add additional assertions to validate the response data or behavior
+
+    # Check that the booking was inserted into the database
+    booking = json.loads(response.get_data(as_text=True))
+    print(booking)
+
+    assert booking is not None
+    assert booking[0]["status"] == 'Accepted by Driver'
+    assert booking[0]["user_id"] == 6
+    assert booking[0]["source"] == 'Manchester'
